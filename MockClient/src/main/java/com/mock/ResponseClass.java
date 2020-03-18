aac2db4a8754e08b4b9ac6f7b72d23ea49e6a6eb
priyankagupta21 <51402631+priyankagupta21@users.noreply.github.com>
2020-03-18 10:59:43 +0530

    Updating ReponseClass.

--- MockClient/src/main/java/com/mock/ResponseClass.java
+++ MockClient/src/main/java/com/mock/ResponseClass.java
@@ -17,33 +17,5 @@ import org.apache.http.util.EntityUtils;
  *
  */
 public class ResponseClass {
-	public void getData() throws IOException
-	{ 
-		 
-		 HttpGet request = new HttpGet("http://localhost:8091/rest/api/2/search");
-
-	     // add request headers
-		/*
-		 * request.addHeader("custom-key", "abc");
-		 * request.addHeader(HttpHeaders.USER_AGENT, "Googlebot");
-		 */
-
-	     try (CloseableHttpClient httpClient = HttpClients.createDefault();
-	          CloseableHttpResponse response = httpClient.execute(request)) {
-
-	         // Get HttpResponse Status
-	         System.out.println(response.getProtocolVersion());              // HTTP/1.1
-	         System.out.println(response.getStatusLine().getStatusCode());   // 200
-	         System.out.println(response.getStatusLine().getReasonPhrase()); // OK
-	         System.out.println(response.getStatusLine().toString());        // HTTP/1.1 200 OK
-
-	         HttpEntity entity = response.getEntity();
-	         
-	         if (entity != null) {
-	             // return it as a String
-	             String result = EntityUtils.toString(entity);
-	             System.out.println(result);
-	}
-}
-	}
+	IssuesAttribute issuesAttributeInstance;
 }
