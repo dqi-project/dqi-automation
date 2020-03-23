4fa7eaba61c662e582271bc67df7cc2d3c6daaf0
priyankagupta21 <51402631+priyankagupta21@users.noreply.github.com>
2020-03-23 18:08:23 +0530

    timeoriginalestimate and assignee fields added.

--- DQI-Integration/src/main/java/com/dqi/jira/consumer/Fields.java
+++ DQI-Integration/src/main/java/com/dqi/jira/consumer/Fields.java
@@ -1,9 +1,10 @@
 package com.dqi.jira.consumer;
 
 public class Fields {
-	
 	String summary;
 	String timespent;
+	String timeoriginalestimate;
+	Assignee assignee;
 	
 	public String getSummary() {
 		return summary;
@@ -17,12 +18,22 @@ public class Fields {
 	public void setTimespent(String timespent) {
 		this.timespent = timespent;
 	}
+	
+	public String getTimeoriginalestimate() {
+		return timeoriginalestimate;
+	}
+	public void setTimeoriginalestimate(String timeoriginalestimate) {
+		this.timeoriginalestimate = timeoriginalestimate;
+	}	
+	public Assignee getAssignee() {
+		return assignee;
+	}
+	public void setAssignee(Assignee assignee) {
+		this.assignee = assignee;
+	}
 	@Override
 	public String toString() {
-		return "Fields [summary=" + summary + ", timespent=" + timespent + "]";
+		return "Fields [summary=" + summary + ", timespent=" + timespent + ", timeoriginalestimate="
+				+ timeoriginalestimate + ", assignee=" + assignee + "]";
 	}
-	
-	
-
-
 }
