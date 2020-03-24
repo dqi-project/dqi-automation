2f5b95f61bb09f3930a0be8851549aa2fdedd913
priyankagupta21 <51402631+priyankagupta21@users.noreply.github.com>
2020-03-24 15:16:52 +0530

    Add files via upload

---
+++ DQI-Integration/src/main/java/com/dqi/common/ApplicationConstants.java
@@ -0,0 +1,7 @@
+package com.dqi.common;
+
+public class ApplicationConstants {
+public static final String HOST = "localhost";
+public static final String PORT="8091";
+public static final String API_URL="/rest/api/2/search";
+}
