02c44a57504f48fb15299e996366aba1a554afc4
mohitsrigit <Mohit_Srivastava@symantec.com>
2020-04-14 12:52:38 +0530

    Adding basic auth

--- DQI-Integration/src/main/java/com/dqi/common/ApplicationConstants.java
+++ DQI-Integration/src/main/java/com/dqi/common/ApplicationConstants.java
@@ -1,7 +1,9 @@
 package com.dqi.common;
 
 public class ApplicationConstants {
-public static final String HOST = "localhost";
-public static final String PORT="8091";
-public static final String API_URL="/rest/api/2/search";
+	
+	public static final String HOST = "localhost";
+	public static final String PORT="8091";
+	public static final String API_URL="/rest/api/2/search";
+	
 }
--- DQI-Integration/src/main/java/com/dqi/jira/consumer/JIRAServiceConsumer.java
+++ DQI-Integration/src/main/java/com/dqi/jira/consumer/JIRAServiceConsumer.java
@@ -1,9 +1,16 @@
 package com.dqi.jira.consumer;
 
+import java.nio.charset.Charset;
 import java.util.Arrays;
 
+import javax.xml.bind.DatatypeConverter;
+
+import org.apache.tomcat.util.codec.binary.Base64;
 import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
@@ -40,11 +47,28 @@ public class JIRAServiceConsumer {
 		mappingJackson2HttpMessageConverter.setSupportedMediaTypes(Arrays.asList(MediaType.APPLICATION_JSON, MediaType.APPLICATION_OCTET_STREAM));
 		restTemplate.getMessageConverters().add(mappingJackson2HttpMessageConverter);
 		
-		
-		HttpEntity<SearchRequest> request = new HttpEntity<>(requestVO);
+		String username = "test";
+		String password = "test";
+        
+        String authorizationHeader = "Basic " + DatatypeConverter.printBase64Binary((username + ":" + password).getBytes());
+
+        HttpHeaders requestHeaders = new HttpHeaders();
+        //set up HTTP Basic Authentication Header
+        requestHeaders.add("Authorization", authorizationHeader);
+        requestHeaders.add("Accept", MediaType.APPLICATION_JSON_VALUE);
+        requestHeaders.add("Content-Type", MediaType.APPLICATION_JSON_VALUE);
+
+        //request entity is created with request headers
+        HttpEntity<SearchRequest> requestEntity = new HttpEntity<>(requestVO, requestHeaders);
+        
+        //String url = "http://"+ApplicationConstants.HOST+":"+ApplicationConstants.PORT+ApplicationConstants.API_URL;
+        //String url = "";
+        
 		//String response = restTemplate.postForObject("http://localhost:8091/rest/api/2/search", request, String.class);
-		SearchResponse response = restTemplate.postForObject("http://"+ApplicationConstants.HOST+":"+ApplicationConstants.PORT+ApplicationConstants.API_URL, request, SearchResponse.class);
-		System.out.println(response);
+		SearchResponse response = restTemplate.postForObject("http://"+ApplicationConstants.HOST+":"+ApplicationConstants.PORT+ApplicationConstants.API_URL, requestVO, SearchResponse.class);
+        //ResponseEntity<SearchResponse> responseEntity = restTemplate.exchange(url, HttpMethod.POST, requestEntity, SearchResponse.class);
+		//System.out.println(responseEntity.getBody().toString());
+		System.out.println(response.toString());
 	}
 
 
