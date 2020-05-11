e88750dcbb0b6519a61b33b1db0b6d7ad404f09b
priyankagupta21 <51402631+priyankagupta21@users.noreply.github.com>
2020-05-11 20:51:15 +0530

    Update XLMapperProcessor.java

--- DQI-XLMapper/src/main/java/com/dqi/xlmapper/process/XLMapperProcessor.java
+++ DQI-XLMapper/src/main/java/com/dqi/xlmapper/process/XLMapperProcessor.java
@@ -1,12 +1,16 @@
 package com.dqi.xlmapper.process;
 
-import java.io.IOException;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.InputStream;
 import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.stereotype.Component;
+import org.yaml.snakeyaml.Yaml;
+import org.yaml.snakeyaml.constructor.Constructor;
 
 import com.dqi.xlmapper.config.model.SourDestSheet;
 import com.dqi.xlmapper.config.model.XLMappingCellDefinition;
@@ -14,16 +18,21 @@ import com.dqi.xlmapper.config.model.XLMappingConfigs;
 import com.dqi.xlmapper.core.SourceToTargetCellWriter;
 import com.dqi.xlmapper.core.XLCoordinatesResolver;
 
+
 @Component
 @Configuration
+
 public class XLMapperProcessor {
 @Autowired
 XLMappingConfigs xlmappingConfigs;
 
-
-public void process()
-{
-	System.out.println(xlmappingConfigs);
+public void process(String filePath) 
+{			
+	try (final InputStream in = new FileInputStream(new File(filePath)))
+	{
+		    final Yaml yaml = new Yaml(new Constructor(XLMappingConfigs.class));
+		    xlmappingConfigs=(XLMappingConfigs) yaml.load(in);
+	
 	List<XLMappingCellDefinition> rowCells[] = new ArrayList[2];
 	rowCells[0]=new ArrayList<XLMappingCellDefinition>();
 	rowCells[1]=new ArrayList<XLMappingCellDefinition>();
@@ -31,7 +40,6 @@ public void process()
 	int i=0;
 	
 	if(xlmappingConfigs.getTabWiseMappingDetails()!=null) {
-		System.out.println("xlmappingConfigs.getTabWiseMappingDetails()= "+xlmappingConfigs.getTabWiseMappingDetails());
 		for(SourDestSheet sds : xlmappingConfigs.getTabWiseMappingDetails()) {			
 			if(sds.getSrcCellTargetCellTypeMappings()!=null) {
 				for(String loc : sds.getSrcCellTargetCellTypeMappings()) {
@@ -42,12 +50,12 @@ public void process()
 		}
 		}
 		
-	try {
-		SourceToTargetCellWriter.write(xlmappingConfigs, rowCells);
-	} catch (IOException e) {
-		// TODO Auto-generated catch block
+	//Writing to the target excel sheet.
+		SourceToTargetCellWriter.write(xlmappingConfigs, rowCells);	
+	}
+	catch (Throwable e) {
+		
 		e.printStackTrace();
 	}
-		 
 	}
 }
