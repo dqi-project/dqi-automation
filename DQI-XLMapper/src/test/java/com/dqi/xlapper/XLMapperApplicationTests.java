c269f39471dd2622f0bc8b953084632b5c8566b4
priyankagupta21 <51402631+priyankagupta21@users.noreply.github.com>
2020-05-15 22:09:00 +0530

    Update and rename WordsheetApplicationTests.java to XLMapperApplicationTests.java

--- DQI-XLMapper/src/test/java/com/dqi/xlapper/XLMapperApplicationTests.java
+++ DQI-XLMapper/src/test/java/com/dqi/xlapper/XLMapperApplicationTests.java
@@ -3,8 +3,10 @@ package com.dqi.xlapper;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
-class WordsheetApplicationTests {
+import com.dqi.xlmapper.XLMapperApplication;
+
+@SpringBootTest(classes= {XLMapperApplication.class})
+class XLMapperApplicationTests {
 
 	@Test
 	void contextLoads() {
