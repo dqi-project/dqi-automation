9c6d13629577e2e33e0b6b862734ef7d3d601196
priyankagupta21 <51402631+priyankagupta21@users.noreply.github.com>
2020-05-15 21:36:49 +0530

    Update SourceToTargetCellWriter.java

--- DQI-XLMapper/src/main/java/com/dqi/xlmapper/core/SourceToTargetCellWriter.java
+++ DQI-XLMapper/src/main/java/com/dqi/xlmapper/core/SourceToTargetCellWriter.java
@@ -2,6 +2,7 @@ package com.dqi.xlmapper.core;
 
 import java.io.File;
 import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.util.List;
@@ -10,108 +11,94 @@ import org.apache.poi.xssf.usermodel.XSSFRow;
 import org.apache.poi.xssf.usermodel.XSSFSheet;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 
-import com.dqi.xlmapper.config.model.SourDestSheet;
+import com.dqi.xlmapper.config.model.TabWiseMappingDetails;
 import com.dqi.xlmapper.config.model.XLMappingCellDefinition;
 import com.dqi.xlmapper.config.model.XLMappingConfigs;
 
 public class SourceToTargetCellWriter {
 public static void write(XLMappingConfigs xlmappingConfigs,List<XLMappingCellDefinition> xlmappingCellDefinitionList[]) throws IOException 
-{
-	String destFile = xlmappingConfigs.getTargetXL();
-	String sourFile = xlmappingConfigs.getSourceXL();
+{	
+
+	try(XSSFWorkbook sourceWorkbook = new XSSFWorkbook(new FileInputStream(new File(xlmappingConfigs.getSourceXL()))))
+	{	
 	
-	//File inputFile = new File("C:\\Users\\abhisheksinghk\\Desktop\\Hcldoc\\S2Result.xlsx");
-	File sourInputFile = new File(sourFile);
-	FileInputStream sourfis = new FileInputStream(sourInputFile);
-	XSSFWorkbook sourWorkbook = new XSSFWorkbook(sourfis);
+	try(XSSFWorkbook targetWorkbook = new XSSFWorkbook(new FileInputStream(new File(xlmappingConfigs.getTargetXL()))))
+	{
 	
-	//File outputFile = new File(
-//			"C:\\Users\\abhisheksinghk\\Desktop\\Hcldoc\\IDM_Data_Collection_Symantec_Dev_Capacity.xlsm");
-	File destOutputFile = new File(destFile);
-	FileInputStream destfis = new FileInputStream(destOutputFile);
-	XSSFWorkbook destWorkbook = new XSSFWorkbook(destfis);
-	
-	//logic
-	String destSheet =null;
-	String sourSheet =null;
-	XSSFSheet sourXSSFSheet =null;
-	XSSFSheet destXSSFSheet = null;
-	XSSFRow sourRow = null ;
-	String sourStrCell = null ;
-	double sourIntCell = 0;
-	XSSFRow destRow = null ;
-	String destCell = null ;
+	XSSFSheet sourceXLSheet =null;
+	XSSFSheet targetXLSheet = null;
+	XSSFRow sourceXLRow = null ;
+	String sourceXLStringCellValue = null ;
+	double sourceXLNumericCellValue = 0.0;
+	XSSFRow targetXLRow = null ;
 	
 	
-	int i=0;
-	for(SourDestSheet sds : xlmappingConfigs.getTabWiseMappingDetails()) {
-		 destSheet = sds.getTargetTab();
-		 sourSheet = sds.getSourceTab();
-		 List<XLMappingCellDefinition> rowCells =xlmappingCellDefinitionList[i];//New List array.
-		 sourXSSFSheet = sourWorkbook.getSheet(sourSheet);
-		 destXSSFSheet = destWorkbook.getSheet(destSheet);
-		 sourRow = null ;
-		 sourStrCell = null ;
-		 destRow = null ;
-		 destCell = null ;
-		 if(rowCells!=null) {
-			 for(XLMappingCellDefinition rowCell : rowCells) {
+	int listIndex=0;
+	for(TabWiseMappingDetails tabWiseMappingDetails : xlmappingConfigs.getTabWiseMappingDetails()) {
+		 
+		 sourceXLSheet = sourceWorkbook.getSheet(tabWiseMappingDetails.getSourceTab());
+		 targetXLSheet = targetWorkbook.getSheet(tabWiseMappingDetails.getTargetTab());
+		 
+		 sourceXLRow = null ;
+		 sourceXLStringCellValue = null ;
+		 targetXLRow = null ;
+		 if(xlmappingCellDefinitionList[listIndex]!=null) {
+			 for(XLMappingCellDefinition xlmappingCellDefinition : xlmappingCellDefinitionList[listIndex]) {
 				 
-				  sourRow = sourXSSFSheet.getRow(rowCell.getSourRow());
-				  if(destXSSFSheet.getRow(rowCell.getDestRow())==null)
-					  destXSSFSheet.createRow(rowCell.getDestRow());
-				 destRow = destXSSFSheet.getRow(rowCell.getDestRow());
-				 System.out.println("Row : "+rowCell.getSourRow()+", cell : "+rowCell.getSourceColumn()+", type : "+rowCell.getValueType()+", sourSheet : "+sourSheet);
-				  if("String".equalsIgnoreCase(rowCell.getValueType())) {
+				  sourceXLRow = sourceXLSheet.getRow(xlmappingCellDefinition.getSourceRow());
+				  if(targetXLSheet.getRow(xlmappingCellDefinition.getTargetRow())==null)
+					  targetXLSheet.createRow(xlmappingCellDefinition.getTargetRow());
+				 targetXLRow = targetXLSheet.getRow(xlmappingCellDefinition.getTargetRow());
+				 System.out.println("Row : "+xlmappingCellDefinition.getSourceRow()+", Column : "+xlmappingCellDefinition.getSourceColumn()+", type : "+xlmappingCellDefinition.getValueType()+", sourceTab : "+tabWiseMappingDetails.getSourceTab());
+				  if("String".equalsIgnoreCase(xlmappingCellDefinition.getValueType())) {
 					  try {
-							sourStrCell = sourRow.getCell(rowCell.getSourceColumn()).getStringCellValue();
-							if(destRow.getCell(rowCell.getTargetColumn())==null)
+							sourceXLStringCellValue = sourceXLRow.getCell(xlmappingCellDefinition.getSourceColumn()).getStringCellValue();
+							if(targetXLRow.getCell(xlmappingCellDefinition.getTargetColumn())==null)
 							{
-								destRow.createCell(rowCell.getTargetColumn());
+								targetXLRow.createCell(xlmappingCellDefinition.getTargetColumn());
 							}
-						  destRow.getCell(rowCell.getTargetColumn()).setCellValue(sourStrCell);
-					} catch (Exception e) {
-						System.out.println("Error : "+e );
-						e.printStackTrace();
+						  targetXLRow.getCell(xlmappingCellDefinition.getTargetColumn()).setCellValue(sourceXLStringCellValue);
+					} catch (IllegalStateException e) {
+						System.out.println("Error : "+ "Found Numeric value, required String value." );
+						throw new IllegalStateException();
+						//e.printStackTrace();
 					}
 				  }
-				  if("NUMERIC".equalsIgnoreCase(rowCell.getValueType())) {
+				  if("NUMERIC".equalsIgnoreCase(xlmappingCellDefinition.getValueType())) {
 				  try {
-					  sourIntCell = sourRow.getCell(rowCell.getSourceColumn()).getNumericCellValue();
-						if(destRow.getCell(rowCell.getTargetColumn())==null)
+					  sourceXLNumericCellValue = sourceXLRow.getCell(xlmappingCellDefinition.getSourceColumn()).getNumericCellValue();
+						if(targetXLRow.getCell(xlmappingCellDefinition.getTargetColumn())==null)
 						{
-							destRow.createCell(rowCell.getTargetColumn());
+							targetXLRow.createCell(xlmappingCellDefinition.getTargetColumn());
 						}
-					  destRow.getCell(rowCell.getTargetColumn()).setCellValue(sourIntCell);
-				} catch (Exception e) {
-					System.out.println("Error : "+e );
-					e.printStackTrace();
+					  targetXLRow.getCell(xlmappingCellDefinition.getTargetColumn()).setCellValue(sourceXLNumericCellValue);
+				} catch (IllegalStateException e) {
+					System.out.println("Error : "+ "Found String value, required Numeric value." );
+					throw new IllegalStateException();
+					//e.printStackTrace();
 				}
 			  }
 			 }
 		 }
-		 i++;
-		
+		 listIndex++;		
+	}	
+
+	try(FileOutputStream fileOutputStream = new FileOutputStream(new File(xlmappingConfigs.getTargetXL())))
+	{
+	targetWorkbook.write(fileOutputStream);
+	}catch(FileNotFoundException e)
+	{
+		System.out.println(xlmappingConfigs.getTargetXL() + " file not found.");
 	}
 	
-	destfis.close();
-	sourfis.close();
-	FileOutputStream destfos = new FileOutputStream(destOutputFile);
-	destWorkbook.write(destfos);
-	sourWorkbook.close();
-	destWorkbook.close();
-	destfos.close();
-//	XSSFSheet setUpXSSFSheet = sourWorkbook.getSheet("SetUp Parameters");
-//	XSSFRow row5 = sourXSSFSheet.getRow(4);
-//	String str = row5.getCell(2).getStringCellValue();
-//	
-//	
-//	XSSFSheet setUpXSSFSheet1 = destWorkbook.getSheet("Setup Parameters");
-//	XSSFRow row17 = destXSSFSheet.getRow(16);
-//	row17.getCell(2).setCellValue(str);
-//	 File outputFile1=new
-//	 File("C:\\Users\\abhisheksinghk\\Desktop\\Hcldoc\\IDM_Data_Collection_Symantec_Dev_Capacity1.xlsm");
-
-	
+	}
+	}catch(FileNotFoundException e)
+	{
+		System.out.println(xlmappingConfigs.getSourceXL() + " file not found");
+	}
+	catch(Throwable e)
+	{
+		System.out.println("An error occurred. Please try again.");
+	}
 }
 }
