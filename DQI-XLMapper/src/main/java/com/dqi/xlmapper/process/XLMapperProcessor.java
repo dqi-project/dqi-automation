2232468fac24c0c57bff37a85c1527d6e8bb6e1d
priyankagupta21 <51402631+priyankagupta21@users.noreply.github.com>
2020-06-01 18:20:34 +0530

    Update XLMapperProcessor.java

--- DQI-XLMapper/src/main/java/com/dqi/xlmapper/process/XLMapperProcessor.java
+++ DQI-XLMapper/src/main/java/com/dqi/xlmapper/process/XLMapperProcessor.java
@@ -7,6 +7,7 @@ import java.io.InputStream;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.apache.log4j.Logger;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.stereotype.Component;
 import org.yaml.snakeyaml.Yaml;
@@ -17,55 +18,65 @@ import com.dqi.xlmapper.config.model.XLMappingCellDefinition;
 import com.dqi.xlmapper.config.model.XLMappingConfigs;
 import com.dqi.xlmapper.core.SourceToTargetCellWriter;
 import com.dqi.xlmapper.core.XLCoordinatesResolver;
-
+import com.dqi.xlmapper.exception.DQIXLMapperInvalidSheetNameException;
+import com.dqi.xlmapper.exception.DQIXLMapperInvalidValueTypeException;
 
 @Component
 @Configuration
 
 public class XLMapperProcessor {
+	private static final Logger logger = Logger.getLogger(XLMapperProcessor.class);
+	public void process(String filePath) throws Throwable 
+	{			
+		try (final InputStream inputStream = new FileInputStream(new File(filePath)))
+		{
+			XLMappingConfigs xlmappingConfigs=(XLMappingConfigs) new Yaml(new Constructor(XLMappingConfigs.class)).load(inputStream);
 
-public void process(String filePath) throws Throwable 
-{			
-	try (final InputStream inputStream = new FileInputStream(new File(filePath)))
-	{
-		XLMappingConfigs xlmappingConfigs=(XLMappingConfigs) new Yaml(new Constructor(XLMappingConfigs.class)).load(inputStream);
-	
-	List<XLMappingCellDefinition> xlmappingCellDefinitionListArray[] = new ArrayList[2];
-	xlmappingCellDefinitionListArray[0]=new ArrayList<XLMappingCellDefinition>();
-	xlmappingCellDefinitionListArray[1]=new ArrayList<XLMappingCellDefinition>();
-	
-	int arrayIndex=0;
-	
-	if(xlmappingConfigs.getTabWiseMappingDetails()!=null) {
-		for(TabWiseMappingDetails tabWiseMappingDetails : xlmappingConfigs.getTabWiseMappingDetails()) {			
-			if(tabWiseMappingDetails.getSrcCellTargetCellTypeMappings()!=null) {
-				for(String srcCellTargetCellTypeMappings : tabWiseMappingDetails.getSrcCellTargetCellTypeMappings()) {
-					xlmappingCellDefinitionListArray[arrayIndex].add(XLCoordinatesResolver.resolve(srcCellTargetCellTypeMappings));					
+			List<XLMappingCellDefinition> xlmappingCellDefinitionListArray[] = new ArrayList[2];
+			xlmappingCellDefinitionListArray[0]=new ArrayList<XLMappingCellDefinition>();
+			xlmappingCellDefinitionListArray[1]=new ArrayList<XLMappingCellDefinition>();
+
+			int arrayIndex=0;
+
+			if(xlmappingConfigs.getTabWiseMappingDetails()!=null) {
+				for(TabWiseMappingDetails tabWiseMappingDetails : xlmappingConfigs.getTabWiseMappingDetails()) {			
+					if(tabWiseMappingDetails.getSrcCellTargetCellTypeMappings()!=null) {
+						for(String srcCellTargetCellTypeMappings : tabWiseMappingDetails.getSrcCellTargetCellTypeMappings()) {
+							xlmappingCellDefinitionListArray[arrayIndex].add(XLCoordinatesResolver.resolve(srcCellTargetCellTypeMappings));					
+						}
+						arrayIndex++;
+					}
 				}
-				arrayIndex++;
 			}
+
+			//Writing to the target excel sheet.
+			SourceToTargetCellWriter.write(xlmappingConfigs, xlmappingCellDefinitionListArray);	
+
 		}
+		catch(DQIXLMapperInvalidValueTypeException e)
+		{
+			logger.error("Exception in XLMapperProcessor class ", e);
+			throw new DQIXLMapperInvalidValueTypeException();
 		}
-		
-	//Writing to the target excel sheet.
-		SourceToTargetCellWriter.write(xlmappingConfigs, xlmappingCellDefinitionListArray);	
-	}
-	catch(FileNotFoundException e)
-	{
-		throw new FileNotFoundException();
-	}
-	catch(IllegalStateException e)
-	{
-		System.out.println("Error occurred. Please check if .yaml file data or excel sheet's data is correct, try again.");
-	}
-	catch(NullPointerException e)
-	{
-		System.out.println("Error occurred. Please check if .yaml file data or excel sheet's data is correct, try again.");
-		//e.printStackTrace();
-	}
-	catch(Throwable e)
-	{
-		throw new Throwable();
-	}
+		catch(DQIXLMapperInvalidSheetNameException e)
+		{
+			logger.error("Exception in XLMapperProcessor class ", e);
+			throw new DQIXLMapperInvalidSheetNameException();
+		}
+		catch(FileNotFoundException e)
+		{
+			logger.error("Exception in XLMapperProcessor class ", e);
+			throw new FileNotFoundException();
+		}
+		catch(IllegalStateException e)
+		{
+			logger.error("Exception in XLMapperProcessor class ", e);
+			throw new IllegalStateException();
+		}
+		catch(Throwable message)
+		{
+			logger.error("Exception in XLMapperProcessor class ", message);
+			throw new Throwable();
+		}
 	}
 }
