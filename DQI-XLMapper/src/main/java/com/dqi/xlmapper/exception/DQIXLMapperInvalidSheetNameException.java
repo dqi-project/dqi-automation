2dea957532c4cd8dbad15863742fddacce13d5dd
priyankagupta21 <51402631+priyankagupta21@users.noreply.github.com>
2020-06-01 18:24:56 +0530

    Add files via upload

---
+++ DQI-XLMapper/src/main/java/com/dqi/xlmapper/exception/DQIXLMapperInvalidSheetNameException.java
@@ -0,0 +1,13 @@
+package com.dqi.xlmapper.exception;
+
+public class DQIXLMapperInvalidSheetNameException extends Exception {
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -4450438929248550400L;
+
+	public DQIXLMapperInvalidSheetNameException() 
+	{
+		super();
+	}
+}
---
+++ DQI-XLMapper/src/main/java/com/dqi/xlmapper/exception/DQIXLMapperInvalidValueTypeException.java
@@ -0,0 +1,13 @@
+package com.dqi.xlmapper.exception;
+
+public class DQIXLMapperInvalidValueTypeException extends Exception {
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -6551408834290518056L;
+
+	public DQIXLMapperInvalidValueTypeException() 
+	{
+		super();
+	}
+}
